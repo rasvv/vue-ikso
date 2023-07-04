3c51ca88fb13cb11ce8945d6960c4a89b8464678
Валерий <64157639+rasvv@users.noreply.github.com>
2023-07-04 21:42:44 +0300

    added jsons

--- src/views/ClientsView.vue
+++ src/views/ClientsView.vue
@@ -1,5 +1,57 @@
 <template>
-  <div class="clients">
-    <h1>This is an clients page</h1>
-  </div>
+			<div class="main__clients spacer">
+				<p><a name="clients"></a></p>
+				<h2>Наши партнеры</h2>
+				<p>Мы сотрудничаем с ведущими производителями резинотехнических изделий России и некоторых иностранных государств</p>
+					<div class="client">
+						<a href="https://www.yart-main.ru">
+							<div>
+								<img src="../assets/img/clients/Yart.png" alt="logo">
+							</div>
+							<p>Ярославль-резинотехника</p>
+						</a>
+						<a href="https://rubexgroup.ru">
+							<div>
+								<img src="../assets/img/clients/RubEx_1.png" alt="logo">	
+							</div>
+							<p>Холдинг RubEx</p>
+						</a>						
+						<a href="http://svarogrvd.ru/">
+							<div>
+								<img src="../assets/img/clients/svarog1.png" alt="logo">
+							</div>
+							<p>"Сварог"</p>
+						</a>	
+						<a href="https://composit.net">
+							<div>
+								<img src="../assets/img/clients/composit.png" alt="logo">
+							</div>
+							<p>НПО "Композит"</p>
+						</a>	
+						<a href="http://www.ptp64.ru">
+							<div>
+								<img src="../assets/img/clients/ptp64.png" alt="logo">
+							</div>
+							<p>ООО "Промтехпласт"</p>
+						</a>							
+						<a href="http://himteks.himsaransk.ru">
+							<div>
+								<img src="../assets/img/clients/Khimtek.png" alt="logo">
+							</div>
+							<p>Химтекс-РТИ</p>
+						</a>	
+						<a href="http://tadem.ru">
+							<div>
+								<img src="../assets/img/clients/tadem.png" alt="logo">
+							</div>
+							<p>Группа компаний "ТАДЕМ"</p>
+						</a>						
+						<a href="https://somkarlas.com">
+							<div>
+								<img src="../assets/img/clients/somkarlas.png" alt="logo">
+							</div>
+							<p>Somkarlas (Turkey)</p>
+						</a>						
+					</div>
+			</div>
 </template>
