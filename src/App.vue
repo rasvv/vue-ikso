5631b91820f38291bc4d44dbdd85dfcd28391a01
Валерий <64157639+rasvv@users.noreply.github.com>
2023-08-18 21:55:50 +0300

    h1 color

--- src/App.vue
+++ src/App.vue
@@ -51,7 +51,7 @@ export default {
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
   text-align: center
-  // color: #2c3e50
+  color: #0f0231
   font-size: 24px
 
 html
